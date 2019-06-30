f3381fc58a2f257d67c46b4e9f7ef7587353cbcc
Will <whof@bu.edu>
2019-06-30 15:59:59 -0400

    html pages are being served by the router

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-var express = require("express");
-// var path = require("path");
+const express = require("express");
+const path = require("path");
 
 // Sets up the Express App
-var app = express();
-var PORT = process.env.PORT || 8080;
+const app = express();
+const PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -13,8 +13,8 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Routes
-// require("./app/routing/htmlRoutes")(app);
-// require("./app/routing/apiRoutes")(app);
+require("./routing/htmlRoutes")(app);
+// require("./routing/apiRoutes")(app);
 
 
 // starting our Express app
