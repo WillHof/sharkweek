01226c6ea6f679a958cd962af14cc6dc86e0a717
Tess Fagan <TessFagan@Tesss-MacBook-Pro.local>
2019-06-30 16:52:21 -0400

    initialized heroku postgres

--- server.js
+++ server.js
@@ -17,7 +17,28 @@ require("./routing/htmlRoutes")(app);
 // require("./routing/apiRoutes")(app);
 
 
+// Heroku Postgres
+const { Client } = require('pg');
+
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+    if (err) throw err;
+    for (let row of res.rows) {
+        console.log(JSON.stringify(row));
+    }
+    client.end();
+});
+
+
 // starting our Express app
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
 });
+
+
