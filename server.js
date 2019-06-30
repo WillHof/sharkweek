f0648d8b5dedc31131e87d9b14a856e9eb81d6eb
Tess Fagan <TessFagan@Tesss-MacBook-Pro.local>
2019-06-30 17:44:03 -0400

    working on setting up DB

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const path = require("path");
+const dotenv = require("dotenv")
+const pg = require("pg")
+
+dotenv.config()
 
 // Sets up the Express App
 const app = express();
@@ -21,19 +25,28 @@ require("./routing/htmlRoutes")(app);
 const { Client } = require('pg');
 
 const client = new Client({
-    connectionString: process.env.DATABASE_URL,
-    ssl: true,
+    connectionString: `${process.env.DATABASE_URL}`,
+    ssl: false,
 });
 
 client.connect();
 
-client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-    if (err) throw err;
-    for (let row of res.rows) {
-        console.log(JSON.stringify(row));
-    }
-    client.end();
-});
+// client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+//     if (err) throw err;
+//     for (let row of res.rows) {
+//         console.log(JSON.stringify(row));
+//     }
+//     client.end();
+// });
+
+// const { Client } = require('pg')
+// const client = new Client()
+
+// await client.connect()
+
+// const res = await client.query('SELECT $1::text as message', ['Hello world!'])
+// console.log(res.rows[0].message) // Hello world!
+// await client.end()
 
 
 // starting our Express app
