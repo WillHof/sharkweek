8838bb11dea1cb1b4419d7922b95af380731cf0b
Will <whof@bu.edu>
2019-07-01 18:18:29 -0400

    testing

--- public/app/app.js
+++ public/app/app.js
@@ -1,8 +1,8 @@
+var userName
 function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    const userName = profile.getName();
-    const userImg = profile.getImageUrl();
-    console.log(userName)
+    let profile = googleUser.getBasicProfile();
+    userName = profile.getName();
+    let userImg = profile.getImageUrl();
     //need to send id token to database
     let id_token = googleUser.getAuthResponse().id_token;
     return userName
