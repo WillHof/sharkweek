fac33b9554b6a531eb934b6116ff3863e6ee79fc
Will <whof@bu.edu>
2019-07-02 18:29:05 -0400

    fixed error

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.static("public"));
 
 app.use(function (req, res, next) {
     console.log(req)
-    console.log(res)
+    next()
     // if (req.session.user === null) {
     //     //if user is not logged-in redirect back to login page //
     //     res.redirect('/');
