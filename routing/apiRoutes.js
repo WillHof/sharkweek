11c21116706061c36edd3856a555fa03853dd701
Tess Fagan <TessFagan@Tesss-MacBook-Pro.local>
2019-07-08 18:37:27 -0400

    testing create

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -19,7 +19,7 @@ module.exports = function (app) {
     //   });
     // });
 
-    app.post("/api/createaccount", function (req, res) {
+    app.post("/api/createAccount", function (req, res) {
         // Create a new User with the data available to us in req.body
         console.log(req.body);
         db.User.create(req.body).then(function (dbUser) {
