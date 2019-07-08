10527542672b284990cfcc5824ba039fdd4aa014
Will <whof@bu.edu>
2019-07-08 19:46:15 -0400

    updated crfeateuserjs file to update the update table

--- public/app/createuser.js
+++ public/app/createuser.js
@@ -2,42 +2,55 @@ $(document).ready(function () {
     // Getting references to the name input and author container, as well as the table body
 
 
-    function upsertUser(userData) {
+    function upsertUser(userData, userUpdate) {
         $.post("/api/createAccount", userData)
             .then(console.log(userData));
     }
 
-    function handleUserCreate(event) {
+    $("#submit").on("click", function (event) {
         event.preventDefault();
         var firstNameInput = $("#firstName").val().trim();
-        console.log(firstNameInput)
         var lastNameInput = $("#lastName").val().trim();
         var email = $("#email").val().trim();
 
         var q1userType = $("#q1").val().trim();
-        // var q2knowAvg = $("#q2").val().trim();
-        // var q3initAvgCycle = $("#q3").val().trim();
-        // var q4initCycleDay = $("#q4").val().trim();
-        // var q5initOnPeriod = $("#q5").val().trim();
-        // var q6initDaysSinceP = $("#q6").val().trim();
-        // Don't do anything if the name fields hasn't been filled out
+        var q2knowAvg = $("#q2").val().trim();
+        var q3initAvgCycle = $("#q3").val().trim();
+        var q4initCycleDay = $("#q4").val().trim();
+        var q5initOnPeriod = $("#q5").val().trim();
+        var q6initDaysSinceP = $("#q6").val().trim();
+
+        let nextDay1
+        //Don't do anything if the name fields hasn't been filled out
         if (!firstNameInput) {
             return;
         }
+        if (q5initOnPeriod = "Yes") {
+            q3initAvgCycle = q6initDaysSinceP + 1
+        }
+
+        if (q2knowAvg === "No" && q5initOnPeriod === "No") {
+            q3initAvgCycle = 28
+            q4initCycleDay = 14
+        }
+        nextDay1 = moment().format().add(1 + q3initAvgCycle - q4initCycleDay, 'days').calendar();
+
 
         // Calling the upsertUser function and passing in the value of the name input
         upsertUser({
             firstName: firstNameInput,
             lastName: lastNameInput,
             email: email,
-            userType: q1userType
-        });
-    }
-
-    $("#submit").on("click", function (event) {
-
-        handleUserCreate(event)
+            userType: q1userType,
+        },
+            {
+                email: email,
+                timeframe: 0,
+                currentAverage: q3initAvgCycle,
+                currentDay: q4initCycleDay,
+                nextPredictedDateOne: nextDay1,
+            });
+    }).then(function () {
+        window.location.href = "./home"
     });
-
-});
-
+})
--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -5,7 +5,8 @@ module.exports = function (app) {
     app.post("/api/createAccount", function (req, res) {
         // Create a new User with the data available to us in req.body
         console.log(req.body);
-        db.User.create(req.body).then(function (dbUser) {
+        db.User.create(req.body).then(db.Update.create(req.body))
+        then(function (dbUser) {
             res.json(dbUser);
         });
     });
