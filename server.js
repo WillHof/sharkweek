9f1a3186ce0913a499d168691983018dab3490fb
Will <whof@bu.edu>
2019-07-08 17:30:03 -0400

    merging

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const path = require("path");
 const dotenv = require("dotenv");
+
+
 // Sets up the Express App
 const app = express();
 const PORT = process.env.PORT || 8080;
+
+
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -12,15 +16,30 @@ app.use(express.json());
 // Static directory
 app.use(express.static("public"));
 
-
 // Routes
-require("./lib/oauth2")
 require("./routing/htmlRoutes")(app);
 // require("./routing/apiRoutes")(app);
 
-// starting our Express app
-app.listen(PORT, function () {
-    console.log("App listening on PORT " + PORT);
+// Requiring our models for syncing
+var db = require("./models");
+
+// sequelize
+db.sequelize
+    .authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+    });
+
+
+// starting our Express app/db
+
+db.sequelize.sync({ force: true }).then(function () {
+    app.listen(PORT, function () {
+        console.log("App listening on PORT " + PORT);
+    });
 });
 
 
