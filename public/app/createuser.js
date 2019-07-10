c6908eeb1b15bda862279d5be3b91d91390a6689
Will <whof@bu.edu>
2019-07-10 13:04:46 -0400

    updated backend code to insert user input into objects for DB insertion

--- public/app/createuser.js
+++ public/app/createuser.js
@@ -6,114 +6,99 @@ $(document).ready(function () {
     $("#Q4").hide();
     $("#submit").hide();
 
-
-
+    var newUserObj = {}
+    var q2Obj = {}
+    var userDataObj = {}
     $("#mainUser").on("click", function (event) {
         event.preventDefault();
-        console.log("clicked main")
-        var q1userType = "mainUser"
-        console.log(q1userType)
+        newUserObj.userType = "mainUser"
         $("#Q2").show();
     });
 
     $("#soFam").on("click", function (event) {
         event.preventDefault();
-        console.log("clicked fam")
-        var q1userType = "soFam"
-        console.log(q1userType)
+        newUserObj.userType = "soFam"
         $("#submit").show();
     });
 
     // Q2
     $("#yes").on("click", function (event) {
         event.preventDefault();
-        console.log("clicked yes")
+        q2Obj.ans = "yes"
         $("#Q3").show();
     });
 
     $("#no").on("click", function (event) {
         event.preventDefault();
-        console.log("clicked no")
+        q2Obj.ans = "no"
         $("#submit").show();
     });
 
     // Q3 avg
     $(".day").on("click", function (event) {
         event.preventDefault();
-        console.log("meow")
-        var q1userType =
-            $("#Q4").show();
+        userDataObj.currentAverage = this.value
+        console.log(userDataObj)
+        $("#Q4").show();
     });
 
     // Q4 cycleday
     $(".cycleday").on("click", function (event) {
         event.preventDefault();
-        console.log("cycle")
+        userDataObj.currentDay = this.value
+        console.log(userDataObj)
         $("#submit").show();
     });
 
-});
-function upsertUser(userData) {
-    $.post("/api/createAccount", userData)
-}
-function upsertUserData(userUpdate) {
-    $.post("/api/createAccountData", userUpdate).then(window.location.href = "./home")
-}
 
-$("#submit").on("click", function (event) {
-    event.preventDefault();
-
-    var firstNameInput = $("#firstName").val().trim();
-    console.log(firstNameInput)
-    var lastNameInput = $("#lastName").val().trim();
-    console.log(lastNameInput)
-    var email = $("#email").val().trim();
-    console.log(email)
-    localStorage.setItem("email", email);
-
-
-
-
-    //are you a main user?
-    var q1userType = $("#q2").val().trim();
-    console.log(q1userType)
-    //do you know your average?
-    var q2knowAvg = $("#q2").val().trim();
-    //what is your average cycle length?
-    var q3initAvgCycle = $("#q3").val().trim();
-    //what day on your cycle are you?
-    var q4initCycleDay = $("#q4").val().trim();
-    // var q5initOnPeriod = $("#q5").val().trim();
-    // var q6initDaysSinceP = $("#q6").val().trim();
-    const today = moment();
-    let nextDay1
-    let actualDay1
-    //Don't do anything if the name fields hasn't been filled out
-    if (!firstNameInput) {
-        return;
+    function upsertUser(userData) {
+        $.post("/api/createAccount", userData)
     }
-    if (q2knowAvg === "No") {
-        q3initAvgCycle = 28
-        q4initCycleDay = 14
+    function upsertUserData(userUpdate) {
+        $.post("/api/createAccountData", userUpdate).then(window.location.href = "./home")
     }
-    //can't use the today constant! it actually changes the moment object itself when we do moment.subtract/moment.add!
-    actualDay1 = moment().subtract(q4initCycleDay, "days").format()
+    function createData(newUserObj1, q2Obj1, userDataObj1) {
+        event.preventDefault();
 
-    nextDay1 = moment().add((1 + (q3initAvgCycle - q4initCycleDay)), "days").format()
-    // Calling the upsertUser function and passing in the value of the name input
-    upsertUser({
-        firstName: firstNameInput,
-        lastName: lastNameInput,
-        email: email,
-        userType: q1userType,
+        newUserObj1.firstName = $("#firstName").val().trim();
+        newUserObj1.lastName = $("#lastName").val().trim();
+        newUserObj1.email = $("#email").val().trim();
+        localStorage.setItem("email", newUserObj1.email);
+
+        //do you know your average?
+        //what is your average cycle length?
+        //what day on your cycle are you?
+        // var q5initOnPeriod = $("#q5").val().trim();
+        // var q6initDaysSinceP = $("#q6").val().trim();
+        const today = moment();
+        let nextDay1
+        let actualDay1
+        //Don't do anything if the name fields hasn't been filled out
+        if (!newUserObj1.firstName) {
+            return;
+        }
+        if (q2Obj1.ans === "No") {
+            userDataObj1.currentAverage = 28
+            userDataObj1.currentDay = 14
+        }
+        //can't use the today constant! it actually changes the moment object itself when we do moment.subtract/moment.add!
+        actualDay1 = moment().subtract(userDataObj1.currentDay, "days").format()
+
+        nextDay1 = moment().add((1 + (userDataObj1.currentAverage - userDataObj1.currentDay)), "days").format()
+        // Calling the upsertUser function and passing in the value of the name input
+        upsertUser(newUserObj1);
+        console.log(userDataObj1)
+        upsertUserData({
+            email: newUserObj1.email,
+            timeframe: 0,
+            currentAverage: userDataObj1.currentAverage,
+            currentDay: userDataObj1.currentDay,
+            nextPredictedDateOne: nextDay1,
+            actualDateOne: actualDay1
+        });
     }
-    );
-    upsertUserData({
-        email: email,
-        timeframe: 0,
-        currentAverage: q3initAvgCycle,
-        currentDay: q4initCycleDay,
-        nextPredictedDateOne: nextDay1,
-        actualDateOne: actualDay1
+
+    $("#submit").on("click", function (event) {
+        createData(newUserObj, q2Obj, userDataObj)
     });
-})
+});
