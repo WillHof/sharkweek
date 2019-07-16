7b52ebc6ee27a5241a1b71dea0d6ffe7d24a5dab
Will <whof@bu.edu>
2019-07-16 00:22:04 -0400

    googleLogin stopped working trying to fix

--- routing/htmlRoutes.js
+++ routing/htmlRoutes.js
@@ -30,6 +30,7 @@ module.exports = function (app) {
             oauth2Client.setCredentials(tokens);
             fs.writeFile("token.json", JSON.stringify(tokens), (err) => {
                 if (err) return console.error(err);
+                res.text("success")
             });
         }
         res.sendFile(path.join(__dirname, "../public/home.html")
